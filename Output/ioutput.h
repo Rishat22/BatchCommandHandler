d0b4edf1dee7a9e63f37304145cc08550f59e245
Zakirov Rishat <zakirovrishat.me@gmail.com>
2021-08-05 17:11:16 +0300

    Revert "remove all files for review"
    This reverts commit de019ab2096a1d379f0e32bba9c761f9f814c962.

---
+++ Output/ioutput.h
@@ -0,0 +1,11 @@
+#ifndef IOUTPUT_H
+#define IOUTPUT_H
+#include <string>
+#include <vector>
+
+class IOutput
+{
+public:
+    virtual void print(const std::vector<std::string> str_data) = 0;
+};
+#endif // IOUTPUT_H
