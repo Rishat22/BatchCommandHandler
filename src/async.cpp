2446f2548b745f18b600bb289c8e4ae5aabc3489
Zakirov Rishat <zakirovrishat.me@gmail.com>
2021-09-01 17:49:52 +0300

    some fix for recommendation

--- src/async.cpp
+++ src/async.cpp
@@ -1,5 +1,7 @@
 #include <memory>
 #include <sstream>
+#include <map>
+#include <ctime>
 #include "async.h"
 #include "outputs/console_output.h"
 #include "outputs/logfile_output.h"
@@ -29,35 +31,47 @@ private:
 	static size_t m_Arg;
 };
 
+int createContext()
+{
+	return 0;
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand();
+	return random_variable;
+}
+
+std::map<int, std::unique_ptr<BatchCommandHandler>> BATCH_COMMAND_HANDLER_LIST;
 
 int connect(const std::size_t bulk_size)
 {
-	Singleton<BatchCommandHandler>::setArgs(bulk_size);
-	auto batch_command_handler = Singleton<BatchCommandHandler>::Instance();
-	batch_command_handler.addOutputPrinter( new ConsoleOutput() );
+//	Singleton<BatchCommandHandler>::setArgs(bulk_size);
+	auto batch_command_handler = std::make_unique<BatchCommandHandler>(bulk_size);
+	batch_command_handler->addOutputPrinter( new ConsoleOutput() );
 	auto logfile_output = new LogFileOutput();
 	logfile_output->startWork();
-	batch_command_handler.addOutputPrinter( logfile_output );
+	batch_command_handler->addOutputPrinter( logfile_output );
 
-	return batch_command_handler.createContext();
+	auto context = createContext();
+	BATCH_COMMAND_HANDLER_LIST[context] = std::move(batch_command_handler);
+	return context;
 }
 
-void receive(const char* data, const std::size_t length, const int /*context*/)
+void receive(const char* data, const std::size_t length, const int context)
 {
 	const auto inputData = std::string{data, length};
 	std::stringstream ss(inputData);
 	std::string command;
 
-	auto batch_command_handler = Singleton<BatchCommandHandler>::Instance();
 	while(std::getline(ss, command, '\n')){
-		batch_command_handler.processCommand(command);
+		BATCH_COMMAND_HANDLER_LIST[context]->processCommand(command);
 	}
 }
 
 void disconnect(const int context)
 {
-	auto batch_command_handler = Singleton<BatchCommandHandler>::Instance();
-	if(batch_command_handler.removeContext(context) )
+	if(BATCH_COMMAND_HANDLER_LIST.find(context) != BATCH_COMMAND_HANDLER_LIST.end() )
+		BATCH_COMMAND_HANDLER_LIST.erase(context);
+
+	if(BATCH_COMMAND_HANDLER_LIST.empty())
 		std::cout << "No more connections" << std::endl;
 }
 
